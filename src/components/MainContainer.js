ab6838053f5b9eb6b8ec84781880a0b12a22fd31
Ajay <ajaypanidapu@gmail.com>
2024-10-30 11:56:53 +0530

    secondary containner

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,23 +2,20 @@ import { useSelector } from "react-redux";
 import VideoBackground from "./VideoBackground";
 import VideoTitle from "./VideoTitle";
 
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
-const MainContainer  =() => {
-    const movies = useSelector( store => store.movies?.nowPlayingMovies)
- 
-    if(movies === null) return ;
+  if (!movies) return;
 
-    const mainMovie = movies[0];
-    console.log(mainMovie);
+  const mainMovie = movies[0];
 
-    const {original_title, overview, id} = mainMovie
+  const { original_title, overview, id } = mainMovie;
 
-    return(
-        <div>
-            <VideoBackground movieId = {id}/>
-            <VideoTitle  title={original_title} overview ={overview}/>
-        </div>
-    )
-}
-
+  return (
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
 export default MainContainer;
